fc8107aad02607afee2cdf3059e9935ed06b62c8
mrGoodWolf <138707027+mrGoodWolf@users.noreply.github.com>
2023-09-17 15:43:45 -0500

    made lua "BBCoreMod.java"

---
+++ src/main/java/mchorse/blockbuster/core/BBCoreMod.lua
@@ -0,0 +1,25 @@
+
+function getASMTransformerClass()
+    {
+        return new String[] {BBCoreClassTransformer.class.getName()};
+    }
+
+    
+function getModContainerClass()
+    {
+        return BBCoreModInfo.class.getName();
+    }
+
+    
+function getSetupClass()
+    {
+        return null;
+    }
+function injectData(Map<String, Object> data)
+    {}
+
+    
+function getAccessTransformerClass()
+    {
+        return null;
+    }
